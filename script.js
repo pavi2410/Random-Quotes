6d335f551e8e4bd2eef00e248de62aec9ce2ba49
Pavitra <pavitragolchha@gmail.com>
2019-09-03 01:38:43 +0530

    Changed Quotes API Service, added GitHub corner, updated PWA manifest

--- script.js
+++ script.js
@@ -1,98 +1,93 @@
 const themes = [
   {
-    "bgClass": "is-primary",
-    "buttonClass": "is-light",
-    "color": "#009e6c",
-    "invert": true
+    'bgClass': 'is-primary',
+    'buttonClass': 'is-light',
+    'color': '#009e6c',
+    'invert': trueFF
   },
   {
-    "bgClass": "is-info",
-    "buttonClass": "is-light",
-    "color": "#04a6d7",
-    "invert": true
+    'bgClass': 'is-info',
+    'buttonClass': 'is-light',
+    'color': '#04a6d7',
+    'invert': true
   },
   {
-    "bgClass": "is-success",
-    "buttonClass": "is-light",
-    "color": "#12af2f",
-    "invert": true
+    'bgClass': 'is-success',
+    'buttonClass': 'is-light',
+    'color': '#12af2f',
+    'invert': true
   },
   {
-    "bgClass": "is-warning",
-    "buttonClass": "is-dark",
-    "color": "#ffaf24",
-    "invert": false
+    'bgClass': 'is-warning',
+    'buttonClass': 'is-dark',
+    'color': '#ffaf24',
+    'invert': false
   },
   {
-    "bgClass": "is-danger",
-    "buttonClass": "is-light",
-    "color": "#ff0561",
-    "invert": true
+    'bgClass': 'is-danger',
+    'buttonClass': 'is-light',
+    'color': '#ff0561',
+    'invert': true
   },
   {
-    "bgClass": "is-light",
-    "buttonClass": "is-dark",
-    "color": "#dfd8d9",
-    "invert": false
+    'bgClass': 'is-light',
+    'buttonClass': 'is-dark',
+    'color': '#dfd8d9',
+    'invert': false
   },
   {
-    "bgClass": "is-dark",
-    "buttonClass": "is-light",
-    "color": "#1f191a",
-    "invert": false
+    'bgClass': 'is-dark',
+    'buttonClass': 'is-light',
+    'color': '#1f191a',
+    'invert': false
   }
 ]
 
 let currentTheme = {}
 
-function load() {
+async function load() {
   document.getElementById('next').classList.add('is-loading')
-  fetch('https://talaikis.com/api/quotes/random/')
-    .then(res => res.json())
-    .then(process)
+  let res = await fetch('https://api.quotable.io/random')
+  process(await res.json())
+  updateTheme()
+  document.getElementById('next').classList.remove('is-loading')
 }
 
-function process(json) {
-  const {
-    quote,
-    author,
-    cat: category
-  } = json
-
+function process({ content: quote, author }) {
   document.getElementById('quote').textContent = quote
   document.getElementById('author').textContent = author
-
-  // hide loading
-  document.getElementById('next').classList.remove('is-loading')
-
-  changeTheme()
-
-  document.getElementById('share').setAttribute(
-    'href',
-    `https://twitter.com/intent/tweet?text=${encodeURI(
-      `"${quote}" - ${author}`
-    )}&hashtags=quotes,${category}`
-  )
+  document.getElementById('share').setAttribute('href', getTweetUrl(`"${quote}" - ${author}`))
 }
 
-function changeTheme() {
-
+function updateTheme() {
   let theme = document.getElementById('theme'),
     nextBtn = document.getElementById('next'),
     shareBtn = document.getElementById('share'),
-    themeColor = document.querySelector("meta[name=theme-color]")
+    themeColor = document.querySelector('meta[name=theme-color]'),
+    githubCorner = document.querySelector('.github-corner > svg')
 
   // remove old theme classes
-  theme.classList.remove(currentTheme["bgClass"])
-  nextBtn.classList.remove(currentTheme["buttonClass"])
+  theme.classList.remove(currentTheme['bgClass'])
+  nextBtn.classList.remove(currentTheme['buttonClass'])
   shareBtn.classList.remove('is-inverted')
 
   // get random theme
-  currentTheme = themes[Math.floor(Math.random() * themes.length)]
+  currentTheme = getRandomTheme()
 
   // add new theme classes
-  themeColor.setAttribute("content", currentTheme["color"]) 
-  theme.classList.add(currentTheme["bgClass"])
-  nextBtn.classList.add(currentTheme["buttonClass"])
-  if (currentTheme["invert"]) shareBtn.classList.add('is-inverted')
+  themeColor.setAttribute('content', currentTheme['color'])
+  theme.classList.add(currentTheme['bgClass'])
+  nextBtn.classList.add(currentTheme['buttonClass'])
+  if (currentTheme['invert']) shareBtn.classList.add('is-inverted')
+  githubCorner.attributeStyleMap.set('fill', '#fff')
+  githubCorner.attributeStyleMap.set('color', currentTheme['color'])
+
+}
+
+function getTweetUrl(text, hashtags = ['quotes']) {
+  return `https://twitter.com/intent/tweet?text=${encodeURI(text)}&hashtags=${hashtags.join(',')}`
+}
+
+function getRandomTheme() {
+  return themes[Math.floor(Math.random() * themes.length)]
 }
