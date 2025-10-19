994fcba0308c80a07df8b3bffc04d218f3ea2ba9
Pavitra Golchha <hello@pavi2410.com>
2025-10-19 22:56:27 +0530

    fmt

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 const themes = [
   {
-    'bgClass': 'is-primary',
-    'buttonClass': 'is-light',
-    'color': '#009e6c',
-    'invert': true
+    bgClass: 'is-primary',
+    buttonClass: 'is-light',
+    color: '#009e6c',
+    invert: true
   },
   {
-    'bgClass': 'is-info',
-    'buttonClass': 'is-light',
-    'color': '#04a6d7',
-    'invert': true
+    bgClass: 'is-info',
+    buttonClass: 'is-light',
+    color: '#04a6d7',
+    invert: true
   },
   {
-    'bgClass': 'is-success',
-    'buttonClass': 'is-light',
-    'color': '#12af2f',
-    'invert': true
+    bgClass: 'is-success',
+    buttonClass: 'is-light',
+    color: '#12af2f',
+    invert: true
   },
   {
-    'bgClass': 'is-warning',
-    'buttonClass': 'is-dark',
-    'color': '#ffaf24',
-    'invert': false
+    bgClass: 'is-warning',
+    buttonClass: 'is-dark',
+    color: '#ffaf24',
+    invert: false
   },
   {
-    'bgClass': 'is-danger',
-    'buttonClass': 'is-light',
-    'color': '#ff0561',
-    'invert': true
+    bgClass: 'is-danger',
+    buttonClass: 'is-light',
+    color: '#ff0561',
+    invert: true
   },
   {
-    'bgClass': 'is-light',
-    'buttonClass': 'is-dark',
-    'color': '#dfd8d9',
-    'invert': false
+    bgClass: 'is-light',
+    buttonClass: 'is-dark',
+    color: '#dfd8d9',
+    invert: false
   },
   {
-    'bgClass': 'is-dark',
-    'buttonClass': 'is-light',
-    'color': '#1f191a',
-    'invert': false
+    bgClass: 'is-dark',
+    buttonClass: 'is-light',
+    color: '#1f191a',
+    invert: false
   }
 ]
 
